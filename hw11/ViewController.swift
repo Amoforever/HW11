7c9231807e1c2a315beb8815e8f4b00be416f27b
Amo Ozmanyan <cool.ozmanyan2013@yandex.ru>
2023-06-18 23:11:00 +0300

    Initial Commit

---
+++ hw11/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hw11
+//
+//  Created by Amahastla on 18.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
