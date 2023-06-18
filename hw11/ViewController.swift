a20a90112c1a7491a536a0f1f07a2b88baef107b
Amo Ozmanyan <cool.ozmanyan2013@yandex.ru>
2023-06-18 23:17:58 +0300

    Creating a background.

--- hw11/ViewController.swift
+++ hw11/ViewController.swift
@@ -8,8 +8,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    private lazy var gradient: CAGradientLayer = {
+            let gradient = CAGradientLayer()
+            gradient.frame = view.bounds
+            gradient.colors = [UIColor.systemBlue.cgColor, UIColor.white.cgColor]
+            gradient.startPoint = CGPoint(x: 0.5, y: 0)
+            gradient.endPoint = CGPoint(x: 0.5, y: 1)
+            return gradient
+        }()
     override func viewDidLoad() {
+        view.layer.insertSublayer(gradient, at: 0)
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
