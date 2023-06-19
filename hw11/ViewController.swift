ba1834dd8d425f5b9212244fa43c7fc5deece3c4
Amo Ozmanyan <cool.ozmanyan2013@yandex.ru>
2023-06-19 03:29:59 +0300

    Creating second textField and first button

--- hw11/ViewController.swift
+++ hw11/ViewController.swift
@@ -7,36 +7,25 @@
 
 import UIKit
 
-extension UITextField {
-    func setLeftIcon(_ image: UIImage) {
-        let iconView = UIImageView(frame: CGRect(x: 30, y: 5, width: 20, height: 20))
-        iconView.image = image
-        let iconConteinerView: UIView = UIView(frame: CGRect(x: 20, y: 0, width: 30, height: 30))
-        iconConteinerView.addSubview(iconView)
-        leftView = iconConteinerView
-        leftViewMode = .always
-    }
-}
 
-extension UITextField {
-    func setRightIcon(_ image: UIImage) {
-        let iconView = UIImageView(frame: CGRect(x: -30, y: 5, width: 20, height: 20))
-        iconView.image = image
-        let iconConteinerView: UIView = UIView(frame: CGRect(x: 20, y: 0, width: 30, height: 30))
-        iconConteinerView.addSubview(iconView)
-        rightView = iconConteinerView
-        rightViewMode = .whileEditing
-    }
-}
+
 class ViewController: UIViewController {
     
+  
     @IBOutlet weak var textField: UITextField!
-   
+    
+    @IBOutlet weak var secondTextField: UITextField!
     @IBOutlet weak var label: UILabel!
     
-    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var button: UIButton!
     
-    @IBOutlet weak var secondImageView: UIImageView!
+    func addLeftImageTo(textField: UITextField, andImage img: UIImage) {
+        let leftImageView = UIImageView(frame: CGRect(x: 0.0, y: 0.0, width: img.size.width, height: img.size.height))
+        leftImageView.image = img
+        textField.leftView = leftImageView
+        textField.leftViewMode = .always
+    }
+    
     private lazy var gradient: CAGradientLayer = {
             let gradient = CAGradientLayer()
             gradient.frame = view.bounds
@@ -48,8 +37,22 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         view.layer.insertSublayer(gradient, at: 0)
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         
+        // Do any additional setup after loading the view.
+        let iconImage = UIImage(named:"icon")
+        addLeftImageTo(textField: textField, andImage: iconImage!)
+        let passwordImage = UIImage(named:"key")
+        addLeftImageTo(textField: secondTextField, andImage: passwordImage!)
+        
+        button.backgroundColor = .blue
+        button.tintColor = .white
+        
+        button.layer.shadowColor = UIColor.black.cgColor
+        button.layer.shadowOpacity = 0.3
+        button.layer.shadowOffset = .zero
+        button.layer.shadowRadius = 10
+        button.layer.shouldRasterize = true
+        button.layer.rasterizationScale = UIScreen.main.scale
     }
 
 }
